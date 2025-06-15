7f358849c79ead8e29e5471c15224d9cd765b2dc
Nitesh Nepali <pingnitesh07@gmail.com>
2025-06-15 16:59:59 +0545

    restructure the project

--- env/dev/output.tf
+++ env/dev/output.tf
@@ -6,16 +6,8 @@ output "public_subnet_id" {
   value = module.vpc.public_subnet_id
 }
 
-output "private_subnet_id" {
-  value = module.vpc.private_subnet_id
-}
-
-
 # output block for security group
 output "web_sg_id" {
   value = module.web_sg.security_group_id
 }
 
-output "db_sg_id" {
-  value = module.db_sg.security_group_id
-}
