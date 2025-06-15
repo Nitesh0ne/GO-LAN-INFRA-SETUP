7cf0c372cbf658ac05ae672015ed1cffe35e1c14
Nitesh Nepali <pingnitesh07@gmail.com>
2025-06-15 16:45:31 +0545

    added backend

--- env/dev/provider.tf
+++ env/dev/provider.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "us-east-1"
+  shared_credentials_files = ["d:\\CRED\\terraform_cred.txt"]
+}
+
+terraform {
+  backend "s3" {
+    bucket = "golang-infra-state-store1010"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+    shared_credentials_files = ["d:\\CRED\\terraform_cred.txt"]
+    
+  }
 }
