7bad0b6a591d62c748e074e784b34861e1c98eba
Nitesh Nepali <pingnitesh07@gmail.com>
2025-06-17 14:07:42 +0545

    boot-strap code added

--- boot-strap/main.tf
+++ boot-strap/main.tf
@@ -1,11 +1,13 @@
 
 provider "vault" {
   address = "http://127.0.0.1:8200"
+
 }
 
 data "vault_generic_secret" "aws_creds" {
   path = "aws-creds/aws"
 }
+
 provider "aws" {
   region     = "us-east-1"
   access_key = data.vault_generic_secret.aws_creds.data["access_key"]
